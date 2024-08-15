104108c73acc44a865d9d962cd0397b48f4247a9
erandi2002 <emunasinghe75@gmail.com>
2024-08-15 16:35:34 +0530

    Finished front-end development of Cafe Website using HTML, CSS, and JavaScript.

---
+++ main.js
@@ -0,0 +1,25 @@
+let search = document.querySelector('.search-box');
+
+document.querySelector('#search-icon').onclick = () => {
+    search.classList.toggle('active');
+    navbar.classList.remove('active');
+}
+
+let navbar = document.querySelector('.navbar');
+
+document.querySelector('#menu-icon').onclick = () => {
+    navbar.classList.toggle('active');
+    search.classList.remove('active');
+}
+
+window.onscroll = () => {
+    navbar.classList.remove('active');
+    search.classList.remove('active');
+}
+
+
+let header = document.querySelector('header');
+
+window.addEventListener('scroll', () => {
+    header.classList.toggle('shadow', window.scrollY > 0);
+});
